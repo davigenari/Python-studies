bd53517fe5082093cbae83e7867592750d966a87
Davi <160342099+davigenari@users.noreply.github.com>
2025-09-16 13:14:19 -0300

    Delete viagem.py

--- viagem.py
+++
@@ -1,7 +0,0 @@
-'''Cálculo do tempo de viagem: Peça a distância a ser percorrida e a velocidade média, depois calcule o tempo da viagem'''
-
-distancia = int(input("Insira a distância a ser percorrida: "))
-vm = int(input("Insira a sua velocidade média: "))
-tempo = distancia / vm
-
-print(f"O tempo da sua viagem é {tempo: .2f} horas")
