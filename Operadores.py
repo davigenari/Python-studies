208b5429313044387f5f34db00c9f86afdbdae90
Davi <160342099+davigenari@users.noreply.github.com>
2025-09-16 13:12:40 -0300

    Delete Operadores.py

--- Operadores.py
+++
@@ -1,5 +0,0 @@
-num1 = float(input("Digite um número: "))
-dobro = float(num1 * 2)
-terça = float(num1 / 3)
-
-print(f"O número é {num1}, o dobro de {num1} é {dobro} e a terça parte de {num1} é {terça: .2f}.")
