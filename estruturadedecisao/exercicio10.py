c9b45f189c2e054266f1453fa1a8674aeeb27940
Davi <160342099+davigenari@users.noreply.github.com>
2025-10-20 12:44:04 -0300

    Add files via upload

---
+++ estruturadedecisao/exercicio10.py
@@ -0,0 +1,17 @@
+"""Solicite um número de 1 a 7 e mostre o dia da semana."""
+
+dia = int(input("Digite um número de 1 a 7 para ver o dia da semana: "))
+if dia == 1:
+    print("O dia 1 é domingo.")
+elif dia == 2:
+    print("O dia 2 é segunda-feira.")
+elif dia == 3:
+    print("O dia 3 é terça-feira.")
+elif dia == 4:
+    print("O dia 4 é quarta-feira.")
+elif dia == 5:
+    print("O dia 5 é quinta-feira.")
+elif dia == 6:
+    print("O dia 6 é sexta-feira.")
+elif dia == 7:
+    print("O dia 7 sábado.")
