238c56e9a848fecfec7e31a9f802213ff07aeebf
Davi <160342099+davigenari@users.noreply.github.com>
2026-03-10 13:45:42 -0300

    Add files via upload

---
+++ loop-for/custom_count.py
@@ -0,0 +1,6 @@
+###Peça ao usuário um número e exiba uma contagem de 1 até esse número.
+
+numero = int(input("Digite um número final para a contagem: "))
+
+for i in range(1, numero + 1):  # De 1 a 10
+    print(i)
